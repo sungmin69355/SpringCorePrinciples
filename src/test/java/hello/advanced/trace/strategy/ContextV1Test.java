f9843060139933168537c911ea582951512c870e
sungmin69355 <sungmin69355@naver.com>
2022-04-27 21:03:34 +0900

    fix: 전략패턴 익명함수, 람다 사용

--- src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -16,6 +16,7 @@ public class ContextV1Test {
         logic2();
     }
     private void logic1() {
+
         long startTime = System.currentTimeMillis();
         //비즈니스 로직 실행
         log.info("비즈니스 로직1 실행");
@@ -24,6 +25,7 @@ public class ContextV1Test {
     }
 
     private void logic2() {
+
         long startTime = System.currentTimeMillis();
         //비즈니스 로직 실행
         log.info("비즈니스 로직2 실행");
@@ -36,12 +38,73 @@ public class ContextV1Test {
      */
     @Test
     void strategyV1() {
+
         Strategy strategyLogic1 = new StrategyLogic1();
         ContextV1 context1 = new ContextV1(strategyLogic1);
         context1.execute();
+
         Strategy strategyLogic2 = new StrategyLogic2();
         ContextV1 context2 = new ContextV1(strategyLogic2);
         context2.execute();
     }
+
+    /**
+     *전략 패턴 익명 내부 클래스1
+     */
+    @Test
+    void strategyV2() {
+
+        Strategy strategyLogic1 = new Strategy() {
+            @Override
+            public void call() { log.info("비즈니스 로직1 실행");
+            }
+        };
+        log.info("strategyLogic1={}", strategyLogic1.getClass());
+        ContextV1 context1 = new ContextV1(strategyLogic1);
+        context1.execute();
+
+        Strategy strategyLogic2 = new Strategy() {
+            @Override
+            public void call() { log.info("비즈니스 로직2 실행");
+            }
+        };
+        log.info("strategyLogic2={}", strategyLogic2.getClass());
+        ContextV1 context2 = new ContextV1(strategyLogic2);
+        context2.execute();
+    }
+
+    /**
+     *전략 패턴 익명 내부 클래스2
+     */
+    @Test
+    void strategyV3() {
+
+        ContextV1 context1 = new ContextV1(new Strategy() {
+            @Override
+            public void call() { log.info("비즈니스 로직1 실행");
+            } });
+        context1.execute();
+
+        ContextV1 context2 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행"); }
+        });
+        context2.execute();
+    }
+
+    /**
+     * 전략 패턴, 람다
+     */
+    @Test
+    void strategyV4() {
+
+        ContextV1 context1 = new ContextV1(() -> log.info("비즈니스 로직1 실행"));
+        context1.execute();
+
+        ContextV1 context2 = new ContextV1(() -> log.info("비즈니스 로직2 실행"));
+        context2.execute();
+    }
+
 
 }
