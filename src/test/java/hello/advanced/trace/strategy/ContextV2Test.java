fab0d28c6c863da601ac4097c57722ad16e4192f
sungmin69355 <sungmin69355@naver.com>
2022-04-28 20:40:23 +0900

    fix: 전략패턴 파라미터로 전달 방식

---
+++ src/test/java/hello/advanced/trace/strategy/ContextV2Test.java
@@ -0,0 +1,33 @@
+package hello.advanced.trace.strategy;
+
+import hello.advanced.trace.strategy.code.ContextV2;
+import hello.advanced.trace.strategy.code.StrategyLogic1;
+import hello.advanced.trace.strategy.code.StrategyLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV2Test {
+
+    /**
+     *전략 패턴 적용
+     */
+    @Test
+    void strategyV1() {
+        ContextV2 context = new ContextV2();
+        //호출할 떄 넘겨줌
+        context.execute(new StrategyLogic1());
+        context.execute(new StrategyLogic2());
+    }
+
+    /**
+     *전략 패턴 익명 내부 클래스2,람다
+     */
+    @Test
+    void strategyV3() {
+        ContextV2 context = new ContextV2();
+        context.execute(() -> log.info("비즈니스 로직1 실행"));
+        context.execute(() -> log.info("비즈니스 로직2 실행"));
+    }
+
+}
